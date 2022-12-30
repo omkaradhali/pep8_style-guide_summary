3719656befd86730353f801453d0be4c2f2ad3b7
Adhali <adhalio@lpth0234.mskcc.root.mskcc.org>
2022-12-29 19:37:47 -0500

    first example code and screenshot

---
+++ code/indentation_example_1.py
@@ -0,0 +1,28 @@
+# Correct:
+
+# Aligned with opening delimiter.
+foo = long_function_name(var_one, var_two,
+                         var_three, var_four)
+
+# Add 4 spaces (an extra level of indentation) to distinguish arguments from the rest.
+
+
+def long_function_name(
+        var_one, var_two, var_three,
+        var_four):
+    print(var_one)
+
+
+# Wrong:
+
+# Arguments on first line forbidden when not using vertical alignment.
+foo = long_function_name(var_one, var_two,
+                         var_three, var_four)
+
+# Further indentation required as indentation is not distinguishable.
+
+
+def long_function_name(
+        var_one, var_two, var_three,
+        var_four):
+    print(var_one)
